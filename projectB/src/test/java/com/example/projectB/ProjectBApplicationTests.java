3490b991e96530801b47c20497701133a5033eee
abr1le@bosch.com <HP@MacBook-Pro.local>
2021-07-12 00:15:01 +0100

    monorepo push

---
+++ projectB/src/test/java/com/example/projectB/ProjectBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
