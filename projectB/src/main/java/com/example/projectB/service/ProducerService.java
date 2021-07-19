89fa2c6abab904d6fc677ea251eb33b1829ccf03
rahmanpq <KaspaBongo123$>
2021-07-19 18:47:49 +0100

    Avro integration with yml schema registry config.

--- projectB/src/main/java/com/example/projectB/service/ProducerService.java
+++ projectB/src/main/java/com/example/projectB/service/ProducerService.java
@@ -1,13 +1,13 @@
 package com.example.projectB.service;
 
 import com.example.projectB.models.Customer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 import static java.lang.String.format;
@@ -15,7 +15,7 @@ import static java.lang.String.format;
 @Service
 public class ProducerService {
     private static final Logger LOGGER = LoggerFactory.getLogger(ProducerService.class);
-    private static final String TOPIC = "userMessage";
+    private static final String TOPIC = "userMessage4";
 
     @Autowired
     private KafkaTemplate<String, Customer> kafkaTemplate;
